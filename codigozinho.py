fab4807f2b791c107cd5ff705f51d0134ae7174e
livingtonfr <livingtonf@discente.ufg.br>
2026-05-07 20:07:33 -0300

    fix: arrumei umas parada

--- codigozinho.py
+++ codigozinho.py
@@ -1,13 +1,16 @@
-import dotenv
+from dotenv import load_dotenv
 import os
 
-x = input("Fala um ponto x:")
-y = input("Fala um ponto y:")
+load_dotenv("config.env")
+import os
+
+x = int(input("Fala um ponto x:"))
+y = int(input("Fala um ponto y:"))
 
 print(f"Que bacana! O seu ponto é o ({x},{y})")
 print(f"A distância dele para o ponto registrado no ambiente é: ", end="")
 
-env_x = os.getenv("PONTO_X")
-env_y = os.getenv("PONTO_Y")
+env_x = int(os.getenv("PONTO_X"))
+env_y = int(os.getenv("PONTO_Y"))
 
-print(((x-env_x)**2 + (y-env_y)**2)**1/2)
+print(((x-env_x)**2 + (y-env_y)**2)**(1/2))
